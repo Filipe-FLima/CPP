8b28eaf68b7021bc92858e952bb0f724d526f9ee
Filipe Lima <filipelimaffl@gmail.com>
2025-07-17 11:17:44 +0200

    done ex00

--- CPP-01/ex00/main.cpp
+++ CPP-01/ex00/main.cpp
@@ -6,71 +6,67 @@
 /*   By: flima <flima@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/10 20:54:51 by flima             #+#    #+#             */
-/*   Updated: 2025/07/14 12:57:45 by flima            ###   ########.fr       */
+/*   Updated: 2025/07/17 11:15:57 by flima            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-int	main(void)
+std::string	getValidInput(std::string prompt)
+{
+	std::string	input;
+
+	while (true)
+	{
+		std::cout << prompt << std::endl;
+		std::getline(std::cin, input);
+		if (!input.empty())
+			return input;
+	}
+}
+
+void	getRandomChump(void)
+{
+	std::string	name = getValidInput("Choose a nice Chump's name");
+	randomChump(name);
+}
+
+void	getNewZombie(void)
 {
 	Zombie*	z;
-	std::string type;
-	std::string name;
 	std::string	kill;
+	
+	std::string	name = getValidInput("Choose a nice Zombie's name");
+	z = newZombie(name);
+	z->announce();
+	while (true)
+	{
+		std::cout << "Now kill it!! (type 'kill')" << std::endl;
+		std::getline(std::cin, kill);
+		if (kill == "kill")
+		{
+			delete z;
+			break;
+		}
+	}
+}
+
+int	main(void)
+{
+	std::string type;
 	
 	while(true)
 	{
-		std::cout << "Pick your zombie type: randomChump, newZombie or EXIT" << std::endl;
+		std::cout << "Create your zombie type: randomChump, newZombie or EXIT" << std::endl;
 		std::getline(std::cin, type);
 		if (type == "randomChump")
-		{
-			while (true)
-			{
-				std::cout << "Choose a nice Chump's name" << std::endl;
-				std::getline(std::cin, name);
-				if (name.empty())
-					continue;
-				else
-				{
-					randomChump(name);
-					break;
-				}
-			}
-			
-		}
+			getRandomChump();
 		else if (type == "newZombie")
-		{
-			while (true)
-			{
-				std::cout << "Choose a nice Zombie's name" << std::endl;
-				std::getline(std::cin, name);
-				if (name.empty())
-					continue;
-				else
-				{
-					z = newZombie(name);
-					z->announce();
-					while (true)
-					{
-						std::cout << "Now kill it!!" << std::endl;
-						std::getline(std::cin, kill);
-						if (kill != "kill")
-							continue;
-						else
-						{
-							delete z;
-							break;
-						}
-					}
-				}
-				break;
-			}
-		}
+			getNewZombie();
 		else if (type == "EXIT")
 			break;
+		else
+			std::cout << "Invalid command. Try again." << std::endl;
 	}
 	return 0;
 }
-
-// break main in a readable code
